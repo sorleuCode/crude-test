99b62ed59b6e0879fc22c516052b2f38842846aa
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2025-07-04 15:52:19 +0100

    update

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.cookies?.token;
+  const token = req.cookies.token;
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
