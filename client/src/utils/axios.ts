963835dde780c703cee7e314fb22a92391e5900c
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2025-07-05 15:34:07 +0100

    update

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://meeting-backend1-production.up.railway.app/", 
+  baseURL: "https://meeting-backend1-production.up.railway.app/api", 
   withCredentials: true, 
 });
 
