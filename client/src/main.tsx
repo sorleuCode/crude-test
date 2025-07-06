7e9e1b99c0615ec279aa64129de0ce8fe6c06768
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2025-07-06 09:48:27 +0100

    update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,12 +5,17 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import "./index.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster"
+
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
       <Provider store={store}>
         <AppRoutes />
+        <Toaster/>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
